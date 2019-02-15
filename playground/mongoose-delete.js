d88d05140a0649d2ef40232001fa97006f7883b8
James Morgan <jmorga11@gmail.com>
2019-02-15 15:48:06 -0800

    Add PATCH todos by id

--- playground/mongoose-delete.js
+++ playground/mongoose-delete.js
@@ -4,9 +4,9 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-Todo.remove({}).then((result) => {
-  console.log(result);
-});
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
 
 Todo.findOneAndRemove('5c673b05334ac37c909b75bc').then((todo) => {
   console.log(todo);
